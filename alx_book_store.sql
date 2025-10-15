e6cb26643f6f281220319aa4e12d80c39f6fb4b7
Motolani <alebiosumotolani@gmail.com>
2025-10-15 01:34:28 +0100

    fixed a mistake

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,7 +22,7 @@ address TEXT,
 
 CREATE TABLE Orders (
 order_id INT PRIMARY KEY AUTO_INCREMENT,
-FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 order_date DATE,
 );
 
